c6410001d4875a555a03ee63159e9c7184364558
MrRonnie <ronymaza55@gmail.com>
2022-03-01 18:57:44 +0600

    2nd Commit

--- JS/app.js
+++ JS/app.js
@@ -11,6 +11,38 @@ const allplayers = () => {
     
 }
 
-const showPlayersDetails = (player) => {
-    console.log(player)
+const showPlayersDetails = (players) => {
+    for (const player of players){
+
+        const parent = document.getElementById('player-container');
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card border p-5 m-2">
+      <div class="pro-pic">
+          <img class="w-25" src="${player.strThumb}" alt="">
+      </div>
+      <h2>Name:${player.strPlayer}</h2>
+      <h4>Country:${player.strNationality} </h4>
+      <p></p>
+      <div class="allbutton">
+          <button class="btn btn-danger">Delete</button>
+          <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+      </div>
+    </div>
+    `;
+    parent.appendChild(div);
+    }
+  
 }
+
+
+const details = (playerId) => {
+    const url = `
+    https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${playerId}
+ 
+    `;
+    fetch(url)
+      .then((response) => response.json())
+      .then ((data) => console.log(data));
+};
